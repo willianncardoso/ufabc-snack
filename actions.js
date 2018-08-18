d690ce746ff0b35bd807e4b72be392bf637b6567
Will <willian.cardoso@icloud.com>
2018-08-18 18:51:01 -0300

    Add files via upload

--- actions.js
+++ actions.js
@@ -18,17 +18,12 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 var popup = L.popup();
 
 function onMapClick(e) {
-
         popup
             .setLatLng(e.latlng)
             .setContent("Charlie")
             .openOn(mymap);
 
-
 }
-
-
-
 
 
 // var geojsonlayer = new L.geoJSON();
@@ -171,15 +166,118 @@ function onMapClick(e) {
 //
 //
 // L.geoJSON(geojsonlayer).addto(mymap);
-function popUp(feature, layer) {
-    layer.bindPopup(feature.properties.show_on_map);
+// function popUp(feature, layer) {
+//     layer.bindPopup(feature.properties.show_on_map).setContent;
+// };
+
+
+
+var geojsonLayer = {"type": "Feature",
+    "properties": {
+        "stroke": "#008040",
+        "stroke-width": 2,
+        "stroke-opacity": 1,
+        "fill": "#408080",
+        "fill-opacity": 2
+    },
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+            [
+                [
+                    -46.52933120727539,
+                    -23.64308927866194
+                ],
+                [
+                    -46.52938485145568,
+                    -23.64346275248227
+                ],
+                [
+                    -46.529545783996575,
+                    -23.643767427704216
+                ],
+                [
+                    -46.530532836914055,
+                    -23.644730589869493
+                ],
+                [
+                    -46.5295135974884,
+                    -23.645094230884396
+                ],
+                [
+                    -46.528977155685425,
+                    -23.645261308849765
+                ],
+                [
+                    -46.52871966361999,
+                    -23.64537924610864
+                ],
+                [
+                    -46.52834415435791,
+                    -23.645526667532778
+                ],
+                [
+                    -46.52814030647278,
+                    -23.645683916868812
+                ],
+                [
+                    -46.527979373931885,
+                    -23.64582150988282
+                ],
+                [
+                    -46.52786135673523,
+                    -23.645968930808706
+                ],
+                [
+                    -46.52766823768616,
+                    -23.64611635156853
+                ],
+                [
+                    -46.527003049850464,
+                    -23.645762541465952
+                ],
+                [
+                    -46.52617692947388,
+                    -23.64528096506696
+                ],
+                [
+                    -46.52618765830994,
+                    -23.64491732457093
+                ],
+                [
+                    -46.52686357498169,
+                    -23.6438263970201
+                ],
+                [
+                    -46.5276038646698,
+                    -23.64363966076201
+                ],
+                [
+                    -46.52851581573486,
+                    -23.64340378300254
+                ],
+                [
+                    -46.52890205383301,
+                    -23.643285843963383
+                ],
+                [
+                    -46.52933120727539,
+                    -23.64308927866194
+                ]
+            ]
+        ]
+    }
 };
 
-var geojsonLayer = new L.GeoJSON.AJAX("https://uploadfiles.io/fwf0i/map(1).geojson", {dataType:"jsonp"}, {onEachFeature:popUp});
 
-mymap.fitBounds(geojsonLayer.getBounds());
-
-geojsonLayer.addTo(map);
-
-
+var geojson = L.geoJson(geojsonLayer, {
+    style: function (feature) {
+        return {
+            weight: 5,
+            color: feature.properties.fill,
+            opacity: 10,
+        };
+    }
+})
+    geojson.addTo(mymap);ß
 
